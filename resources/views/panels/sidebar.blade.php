fc40fa11496506bffd67a6b8f3cee287fa5f64e3
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-07-11 23:02:12 +0100

    fix logo

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -4,14 +4,18 @@
 <div class="main-menu {{ $configData['menuType'] }} {{($configData['theme'] === 'light') ? "menu-light" : "menu-dark"}} menu-accordion menu-shadow" data-scroll-to-active="true">
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mr-auto"><a class="navbar-brand" href="dashboard-analytics">
-                    <div class="brand-logo"></div>
-                    <h2 class="brand-text mb-0">Vuexy</h2>
+            <li class="nav-item mr-auto"><a class="navbar-brand" href="dashboard">
+                    <div class="brand-logo"><img src="{{ asset('images/logo/big-logo.png') }}" alt="logo" height="53"></div>
+                    {{--<h2 class="brand-text mb-0">123MediaHub</h2>--}}
                 </a></li>
+                <li>
+                    <div></div>
+                </li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary collapse-toggle-icon" data-ticon="icon-disc"></i></a></li>
         </ul>
     </div>
     <div class="shadow-bottom"></div>
+    
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
             {{-- Foreach menu item starts --}}
