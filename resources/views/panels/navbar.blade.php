9a7b511aac136b61f4e3be29aa77b3a989af5ad2
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-07-13 17:24:18 +0100

    Add-Up

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -17,13 +17,13 @@
                         <li class="nav-item mobile-menu d-xl-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ficon feather icon-menu"></i></a></li>
                     </ul>
                     <ul class="nav navbar-nav bookmark-icons">
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="sk-layout-2-columns" data-toggle="tooltip" data-placement="top" title="2-Columns"><i class="ficon feather icon-sidebar"></i></a></li>
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link" href="add-media" data-toggle="tooltip" data-placement="top" title="Add Media"><i class="ficon feather icon-music"></i></a></li>
                     </ul>
                     <ul class="nav navbar-nav">
                         <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon feather icon-star warning"></i></a>
                             <div class="bookmark-input search-input">
                                 <div class="bookmark-input-icon"><i class="feather icon-search primary"></i></div>
-                                <input class="form-control input" type="text" placeholder="Explore Vuexy..." tabindex="0" data-search="laravel-starter-list" />
+                                <input class="form-control input" type="text" placeholder="Explore Shortcut..." tabindex="0" data-search="laravel-starter-list" />
                                 <ul class="search-list search-list-bookmark"></ul>
                             </div>
                             <!-- select.bookmark-select-->
@@ -106,10 +106,10 @@
                         </ul>
                     </li>
                     <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">John Doe</span><span class="user-status">Available</span></div><span><img class="round" src="{{asset('images/portrait/small/avatar-s-11.jpg') }}" alt="avatar" height="40" width="40" /></span>
+                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">{{Auth::user()->name}}</span><span class="user-status">Available</span></div><span><img class="round" src="{{asset('images/portrait/small/avatar-s-11.jpg') }}" alt="avatar" height="40" width="40" /></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="javascript:void(0)"><i class="feather icon-user"></i> Edit Profile</a><a class="dropdown-item" href="javascript:void(0)"><i class="feather icon-mail"></i> My Inbox</a><a class="dropdown-item" href="javascript:void(0)"><i class="feather icon-check-square"></i> Task</a><a class="dropdown-item" href="javascript:void(0)"><i class="feather icon-message-square"></i> Chats</a>
-                            <div class="dropdown-divider"></div><a class="dropdown-item" href="javascript:void(0)"><i class="feather icon-power"></i> Logout</a>
+                            <div class="dropdown-divider"></div><a class="dropdown-item" href="logout"><i class="feather icon-power"></i> Logout</a>
                         </div>
                     </li>
                 </ul>
@@ -167,7 +167,7 @@
             <div class="d-flex align-items-center">
                 <div class="avatar mr-50"><img src="{{ asset('images/portrait/small/avatar-s-8.jpg') }}" alt="png" height="32" /></div>
                 <div class="search-data">
-                    <p class="search-data-title mb-0">John Doe</p><small class="text-muted">UI designer</small></div>
+                    <p class="search-data-title mb-0">{{Auth::user()->name}}</p><small class="text-muted">UI designer</small></div>
             </div>
         </a>
     </li>
