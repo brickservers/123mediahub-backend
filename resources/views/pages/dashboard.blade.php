503cd7229cfcd6767326967708f720e994b220ac
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-07-15 13:46:17 +0100

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -195,8 +195,10 @@
 @section('page-script')
         <!-- Page js files -->
         <script src="{{ asset(mix('js/scripts/pages/dashboard-analytics.js')) }}"></script>
+        <script>
         function openIn(url) {
           var win = window.open(url, '_self');
             win.focus();
                 }
+              </script>
 @endsection
